a7d2bb905202eb0e8010db11f6994f0a0619f9b3
bcodrington <bcodrington@uncharted.software>
2023-06-28 14:55:26 -0400

    Add demo input dropdown

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,29 @@ import Head from 'next/head';
 import { useState } from 'react';
 import styles from './index.module.css';
 
+const DEMO_INPUT = [
+  {
+    name: 'Select demo paragraph',
+    input:
+      ""
+  },
+  {
+    name: 'Dogs in restaurants',
+    input:
+      "If we make an exception for Bijal's service dog, then other people will want to bring their dogs. Then everybody will bring their dog, and before you know it, our restaurant will be overrun with dogs, their slobber, their hair, and all the noise they make, and nobody will want to eat here anymore."
+  },
+  {
+    name: 'Marketing strategist',
+    input:
+      "The company increased our marketing budget last week, and we hired a new marketing strategist. Our lead conversion rate increased by 15% from the last week. I believe the new marketing strategist is the reason for the increase in lead conversion."
+  },
+  {
+    name: 'Apples and Restaurants',
+    input:
+      "Apples are red. The restaurant opens at five o'clock and it begins serving between four and nine. Apples are not red."
+  }
+];
+
 export default function Home() {
   const [userInput, setUserInput] = useState('');
   const [result, setResult] = useState([]);
@@ -42,18 +65,24 @@ export default function Home() {
 
       <main className={styles.main}>
         {/* <img src="/dog.png" className={styles.icon} /> */}
-        <h3>Name my pet</h3>
+        <h3>Assess logic</h3>
         <form onSubmit={onSubmit}>
-          <input
-            type="textarea"
+          <textarea
             name="userInput"
             placeholder="Enter a paragraph"
             value={userInput}
             onChange={e => setUserInput(e.target.value)}
-            style={{'height': '400px'}}
+            style={{ height: '200px' }}
           />
-          <input type="submit" value="Generate names" />
+          <input type="submit" value="Analyze" />
         </form>
+        <select onChange={e => setUserInput(e.target.value)}>
+          {DEMO_INPUT.map(input => (
+            <option key={input.input} value={input.input}>
+              {input.name}
+            </option>
+          ))}
+        </select>
         {result.map((statement, index) => (
           <div key={index} className={styles.result}>
             {statement}
